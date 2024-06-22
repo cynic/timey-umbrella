a9085512df738ecb1de6a9c44005fcb6e5ee0f21
Yusuf Motara <y.motara@ru.ac.za>
2024-06-22 15:27:46 +0200

    Insert a default account during development

--- apps/sql_db/priv/repo/seeds.exs
+++ apps/sql_db/priv/repo/seeds.exs
@@ -9,4 +9,11 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-SqlDb.Accounts.User.
+
+# If we're in dev, go ahead and put this in:
+alias SqlDb.Accounts.User
+
+if Mix.env() == :dev do
+  # Create a user
+  SqlDb.Repo.insert!(%User{email: "cinyc.s@gmail.com", hashed_password: "$argon2id$v=19$m=65536,t=8,p=2$MNmRrSGdMi5KuHs5S14bFQ$2m92QT1hF5bI30YOw9WlPdfdFu79+lhKL5yYalxzLZc", confirmed_at: ~N[2024-06-01 00:00:00Z]})
+end
